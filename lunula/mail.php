13a7c177e930142033e96a4fc809652e14dad48a
Vasilii <vas@kabiev.com>
2019-06-10 08:14:42 +0300

    add lunula

---
+++ lunula/mail.php
@@ -0,0 +1,42 @@
+<?php
+
+$to = "bezpalenko.lashtrener@yandex.ru";
+
+$subject = "Сообщение с сайта";
+$email = "vikabezpalenko1@gmail.com ";
+
+if ($_POST) {
+
+	$mess = '';
+
+	if($_POST['name']) $name = trim(strip_tags($_POST['name']));
+	if($_POST['phone']) {
+		$phone = stripslashes($_POST['phone']);
+		$phone = preg_replace('/\s|\+|-|\(|\)/','', $phone);
+	}
+
+	if ($name) {
+		$mess .= "Имя: " . $name . " ";
+	}
+	if ($phone) {
+		$mess .= "Телефон: " . $phone . "\r\n";
+	}
+	/* if ($_POST['typeCourse']) {
+		$mess .= "Курс: " . $_POST['typeCourse'] . "\r\n";
+	} */
+
+	$mail = mail($to, $subject, $mess,
+  	"From: ".$name." <".$email.">\r\n"
+  	."Reply-To: ".$name."<".$email.">\r\n"
+  	);
+
+  	if ($mail) {
+  		echo "Ok";
+  	}
+  	else{
+  		header('HTTP/1.1 500 ' . $error );
+      	exit();
+  	}
+}
+
+?>
