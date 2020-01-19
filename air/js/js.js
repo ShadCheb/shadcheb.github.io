ef4a1fd9227078a8e3c8d76b8527825abc9aea7a
ShadCheb <vas.box@mail.ru>
2020-01-19 17:07:29 +0400

    оптимизация

--- air/js/js.js
+++ air/js/js.js
@@ -261,7 +261,126 @@ function openModal(e) {
 
 
     /*Маска*/
-    $(function(){
-      $(".form__phone").mask("+7 (999) 999-99-99?9");
+    $(".form__phone").mask("+7 (999) 999-99-99?9");
+
+    lightGallery(document.querySelector('#gallery-gallery')); 
+
+    // Видео заставка
+    let videoList = {
+        video1: {
+            idContainer: 'video-1',
+            idVideo: '2GY3KXRt0H0',
+            api: null
+        },
+        video2: {
+            idContainer: 'video-2',
+            idVideo: '2GY3KXRt0H0',
+            api: null
+        },
+        video3: {
+            idContainer: 'video-3',
+            idVideo: '2GY3KXRt0H0',
+            api: null
+        },
+        video4: {
+            idContainer: 'video-4',
+            idVideo: '2GY3KXRt0H0',
+            api: null
+        },
+    };
+    let containerVideo = document.querySelector('.gallery__video-blocks');
+    let templateVideo = document.querySelector('#template-video');
+
+
+    function onYouTubePlayer() {
+        for (let video in videoList) {
+            let idContent = videoList[video].idContainer;
+            let idVideo = videoList[video].idVideo;
+
+            renderTemplateVideo(video);
+
+            runAPIReady(idContent, idVideo, video);
+        }
+
+        $('.video-block__play').on('click', clickPlayVideo);
+    };
+
+    function renderTemplateVideo(video) {
+        let element;
+
+        if('content' in templateVideo){
+            element = templateVideo.content.children[0].cloneNode(true);
+        } else{
+            element = templateVideo.children[0].cloneNode(true);
+        }
+
+        element.dataset.video = video;
+        element.querySelector('.video-block__video').id = videoList[video].idContainer;
+
+        containerVideo.appendChild(element);
+    }
+
+    function clickPlayVideo(e) {
+        let number = e.target.parentElement.dataset.video;
+
+        this.classList.add('hidden');
+        this.parentElement.classList.add('play');
+
+        videoList[number].api.playVideo();
+    }
+
+    function runAPIReady(container, video, number) {
+        videoList[number].api = new window['YT'].Player(container, {
+            videoId: video,
+            playerVars: {
+                // controls: 0,
+                modestbranding: 1,
+                rel: 0,
+            },
+            events: {
+                'onPlay': clickPlayVideo
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        function loadPlayer() {
+            if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
+                let scriptYT = document.createElement("script");
+      
+                scriptYT.src = "https://www.youtube.com/iframe_api";
+                let firstScriptTag = document.getElementsByTagName("script")[0];
+
+                firstScriptTag.parentNode.insertBefore(scriptYT, firstScriptTag);
+
+                window.onYouTubePlayerAPIReady = function () {
+                    onYouTubePlayer();
+                };
+            }
+        }
+
+        $(function () {
+            loadPlayer();
+        })
+    });
+
+
+    let linkStock = document.querySelector('.link-stock');
+    let stock = document.querySelector('.stock');
+    let stockBlock = document.querySelector('.stock__blocks');
+    let closeStock = document.querySelector('.stock__close');
+
+    linkStock.addEventListener('click', () => {
+      stock.classList.add('active');
+    });
+    stock.addEventListener('click', () => {
+      stock.classList.remove('active');
+    });
+    stockBlock.addEventListener('click', (e) => {
+      if (!e.target.classList.contains('stock__close'))
+        e.stopPropagation();
+    });
+    closeStock.addEventListener('', () => {
+      stock.classList.remove('active');
     });
 }());
